f8843c7687e82ffa2bf7ce3a94e035ffd11d6cc2
udit-chourasia <chourasiaudit@gmail.com>
2016-04-01 12:52:12 +0530

    added Dealer,Product,Vendor html pages.

---
+++ SMSPortal/Controllers/PostLogin/DealerController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SMSPortal.Controllers.PostLogin
+{
+    public class DealerController : Controller
+    {
+        //
+        // GET: /Dealer/
+        public ActionResult Search()
+        {
+            return View("Search");
+        }
+
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+    }
+}
---
+++ SMSPortal/Controllers/PostLogin/VendorController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SMSPortal.Controllers.PostLogin
+{
+    public class VendorController : Controller
+    {
+        //
+        // GET: /Vendor/
+        public ActionResult Search()
+        {
+            return View("Search");
+        }
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+    }
+}
