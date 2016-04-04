02bc6dacd3b2e505e8f6c077b1d72ab92b7e2c9b
prasad-borade <pborade1994@gmail.com>
2016-04-04 15:08:00 +0530

    add purchase order page.

---
+++ SMSPortal/Scripts/Module/PurchaseOrder/PurchaseOrder_events.js
@@ -0,0 +1,22 @@
+﻿$(function () {
+
+    $(".fa-chevron-left").click(function () {
+
+        $("#frmPurchaseOrderMaster").attr("action", "/PurchaseOrder/Search/");
+
+        $("#frmPurchaseOrderMaster").attr("method", "POST");
+
+        $("#frmPurchaseOrderMaster").submit();
+
+    });
+
+});
+$(document).ready(function () {
+
+    $('input:not(.non-iCheck input:checkbox)').iCheck({
+        checkboxClass: 'icheckbox_square-green',
+        radioClass: 'iradio_square-green',
+        increaseArea: '20%' // optional
+    });
+
+});
