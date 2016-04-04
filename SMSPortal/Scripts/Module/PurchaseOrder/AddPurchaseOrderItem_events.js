166964ea850b439bc04f1f8ce31f291250c8d6d8
prasad-borade <pborade1994@gmail.com>
2016-04-04 17:46:13 +0530

    add purchase order item

---
+++ SMSPortal/Scripts/Module/PurchaseOrder/AddPurchaseOrderItem_events.js
@@ -0,0 +1,13 @@
+﻿$(function () {
+
+    $(".fa-chevron-left").click(function () {
+
+        $("#frmPurchaseOrderItem").attr("action", "/PurchaseOrder/Search/");
+
+        $("#frmPurchaseOrderItem").attr("method", "POST");
+
+        $("#frmPurchaseOrderItem").submit();
+
+    });
+
+});
