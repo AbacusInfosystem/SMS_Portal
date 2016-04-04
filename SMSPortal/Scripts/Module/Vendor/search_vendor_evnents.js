33bd3b053fc8eec11ebf9af35b4e7fb7e60acf67
sudhakar-patil <patilsudhakar.m@gmail.com>
2016-04-04 14:43:58 +0530

    Search_vendor_event added popup for product mapping

--- SMSPortal/Scripts/Module/Vendor/search_vendor_evnents.js
+++ SMSPortal/Scripts/Module/Vendor/search_vendor_evnents.js
@@ -5,4 +5,9 @@
         $("#div_Parent_Modal_Fade").find(".modal-body").load("/vendor/Add_Bank_Details", {}, call_back);
     });
 
+    $("#btnAddProductMapping").click(function (event) {
+
+        $("#div_Parent_Modal_Fade").find(".modal-body").load("/vendor/Add_Product_Mapping", {}, call_back);
+    });
+
 });
