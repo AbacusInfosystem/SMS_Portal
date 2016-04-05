d9992488e8dc33192d71225ff41ac1cd17723902
sanket_mayekar <sanket.mayekar@abacusinfosystem.com>
2016-04-05 14:33:13 +0530

    Added product image changes.

--- SMSPortal/Controllers/PostLogin/VendorController.cs
+++ SMSPortal/Controllers/PostLogin/VendorController.cs
@@ -29,5 +29,10 @@ namespace SMSPortal.Controllers.PostLogin
             return PartialView("_AddBankDetails");
         }
 
+        public ActionResult Profile()
+        {
+            return PartialView("VendorProfileDetails");
+        }
+
     }
 }
