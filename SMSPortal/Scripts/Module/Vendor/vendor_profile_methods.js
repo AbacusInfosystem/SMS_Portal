e3065836730071ffb07206893c6a1a1c21705523
sanket_mayekar <sanket.mayekar@abacusinfosystem.com>
2016-04-06 20:25:15 +0530

    Done changes in vendor profile.

--- SMSPortal/Scripts/Module/Vendor/vendor_profile_methods.js
+++ SMSPortal/Scripts/Module/Vendor/vendor_profile_methods.js
@@ -2,6 +2,6 @@
 
     $('#div_Parent_Modal_Fade').modal('show');
 
-    $("#div_Parent_Modal_Fade").find(".modal-title").text("Edit Bank Details");
+    $("#div_Parent_Modal_Fade").find(".modal-title").text("Add Bank Details");
 
 }
