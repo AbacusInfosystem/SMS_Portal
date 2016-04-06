f026f26a186b210a7e7fcf3c4f90679f257854b1
prasad-borade <pborade1994@gmail.com>
2016-04-06 19:05:24 +0530

    Added banking details popup in vendor profile.

--- SMSPortal/Scripts/Module/Vendor/vendor_profile_events.js
+++ SMSPortal/Scripts/Module/Vendor/vendor_profile_events.js
@@ -2,7 +2,7 @@
 $(function () {
 
     $("#btnEdit").click(function (event) {
-        alert(1)
+      
         $("#div_Parent_Modal_Fade").find(".modal-body").load("/vendor/Add_Bank_Details", {}, call_back);
     });
 
