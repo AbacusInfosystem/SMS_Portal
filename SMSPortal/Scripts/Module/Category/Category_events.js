24edaea4aedf20a5d96f979044e5e7d2d15f0d90
prasad-borade <pborade1994@gmail.com>
2016-04-06 15:15:49 +0530

    Added category master

---
+++ SMSPortal/Scripts/Module/Category/Category_events.js
@@ -0,0 +1,22 @@
+﻿$(function () {
+
+    $(".fa-chevron-left").click(function () {
+
+        $("#frmCategoryMaster").attr("action", "/Category/Search/");
+
+        $("#frmCategoryMaster").attr("method", "POST");
+
+        $("#frmCategoryMaster").submit();
+
+    });
+
+});
+$(document).ready(function () {
+
+    $('input:not(.non-iCheck input:checkbox)').iCheck({
+        checkboxClass: 'icheckbox_square-green',
+        radioClass: 'iradio_square-green',
+        increaseArea: '20%' // optional
+    });
+
+});
