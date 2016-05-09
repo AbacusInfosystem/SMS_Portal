4ff286c96eb425789a410615b371713b95ddb8cc
prasad-borade <pborade1994@gmail.com>
2016-05-09 14:12:36 +0530

    Added changes for vendor , brand.

--- SMSPortal/Scripts/Module/Vendor/vendor_mapping_events.js
+++ SMSPortal/Scripts/Module/Vendor/vendor_mapping_events.js
@@ -7,6 +7,7 @@
 
     });
 
+
     $("#btnSave").click(function () {
 
         var strStates = "";
@@ -23,7 +24,18 @@
         $("#frmAddProductMapping").attr("action", "/Vendor/insert-vendor-product-mapping-details/");
         $('#frmAddProductMapping').attr("method", "POST");
         $('#frmAddProductMapping').submit();
-    });  
+    });
+
+    $(".fa-chevron-left").click(function () {
+        $("#frmAddProductMapping").validate().cancelSubmit = true;
+
+        $("#frmAddProductMapping").attr("action", "/Vendor/Search/");
+
+        $("#frmAddProductMapping").attr("method", "POST");
+
+        $("#frmAddProductMapping").submit();
+
+    });
 });
  
 
