4c65baebcbef394922f41ed41fda1e09cc8cc068
sudhakar-patil <patilsudhakar.m@gmail.com>
2016-05-18 14:04:54 +0530

    Changes in brand-dealer percentage

--- SMSPortal/Scripts/Module/Dealer/Dealer_events.js
+++ SMSPortal/Scripts/Module/Dealer/Dealer_events.js
@@ -56,44 +56,45 @@ function Calc_Percentage(obj) {
     var txtName = obj.id;
     
     if (txtName == 'txtDealerPercentage')
-    {        
-        if (parseFloat(obj.value) < 100) {
-            $('#err_dealer').text("");
-            var dealer_percetage = parseFloat(obj.value);
-            var brand_val = 0;
-            if ($('#txtBrandPercentage').val() != "") {
-                brand_val = 0;
+    {
+        if (obj.value != "") {
+            if (parseFloat(obj.value) < 100) {
+                $('#err_dealer').text("");
+                var dealer_percetage = parseFloat(obj.value);
+                var brand_val = 0;
+                if ($('#txtBrandPercentage').val() != "") {
+                    brand_val = 0;
+                }
+                else {
+                    brand_val = parseFloat($('#txtBrandPercentage').val());
+                }
+                brand_val = 100 - dealer_percetage;
+                $('#txtBrandPercentage').val(brand_val);
             }
             else {
-                brand_val = parseFloat($('#txtBrandPercentage').val());
+                $('#err_dealer').text('Percentage must be less than 100');
             }
-            brand_val = 100 - dealer_percetage;
-            $('#txtBrandPercentage').val(brand_val);
-        }
-        else
-        {
-            $('#err_dealer').text('Percentage must be less than 100');
         }
     }
     if (txtName == 'txtBrandPercentage')
     {
-        
-        if (parseFloat(obj.value) < 100) {
-            $('#err_brand').text("");
-            var brand_percetage = parseFloat(obj.value);
-            var dealer_val = 0;
-            if ($('#txtDealerPercentage').val() != "") {
-                dealer_val = 0;
+        if (obj.value != "") {
+            if (parseFloat(obj.value) < 100) {
+                $('#err_brand').text("");
+                var brand_percetage = parseFloat(obj.value);
+                var dealer_val = 0;
+                if ($('#txtDealerPercentage').val() != "") {
+                    dealer_val = 0;
+                }
+                else {
+                    dealer_val = parseFloat($('#txtDealerPercentage').val());
+                }
+                dealer_val = 100 - brand_percetage;
+                $('#txtDealerPercentage').val(dealer_val);
             }
             else {
-                dealer_val = parseFloat($('#txtDealerPercentage').val());
+                $('#err_brand').text('Percentage must be less than 100');
             }
-            dealer_val = 100 - brand_percetage;
-            $('#txtDealerPercentage').val(dealer_val);
-        }
-        else
-        {
-            $('#err_brand').text('Percentage must be less than 100');
         }
     }
 
