fc68fb711d063c5201812c5d66a0606cb1589c4f
shakti-pawar <shakti.pawar@abacusinfosystem.com>
2016-07-27 17:28:44 +0530

    Windows Service Integrated in Solution

---
+++ SMSEmailWorker/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMSEmailWorker
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            //ServiceBase[] ServicesToRun;
+            //ServicesToRun = new ServiceBase[] 
+            //{ 
+            //    new SMSEmailWorker() 
+            //};
+            //ServiceBase.Run(ServicesToRun);
+
+            try
+            {
+                SMSEmailWorker sv = new SMSEmailWorker();
+
+                //sv.Send_Mail();
+            }
+            catch (Exception ex)
+            {
+                
+            }
+        }
+    }
+}
